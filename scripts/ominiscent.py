6777832a2f958963ce641a087efa2d8ccc05a11d
ApprehensiveKnee <edoardo.cabiati@mail.polimi.it>
2024-03-05 22:09:59 +0100

    p

--- scripts/ominiscent.py
+++ scripts/ominiscent.py
@@ -285,7 +285,7 @@ def run_box():
     # Create a list for the boxes
     for i in range(len(boxes)):
         os.system("box["+str(i)+"]="+box_string(boxes[i]))
-        os.system ("echo ${box["+str(i)+"]}")
+        os.system ("echo ${box[1]}")
     os.environ["cells"] = cells_string(cells_0)
     os.environ["refinement_boxes"] = refinement_boxes_string(refinement_boxes_0)
     os.environ["refinement_train"] = refinement_train_string(refinement_train_0)
@@ -315,7 +315,7 @@ def run_box():
     ''')
 
     # Clean the environment
-    os.environ["box_@"] = ""
+    os.environ["box"] = ""
     os.environ["cells"] = ""
     os.environ["refinement_boxes"] = ""
     os.environ["refinement_train"] = ""
